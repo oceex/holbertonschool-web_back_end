825717a31aee21a8966a2e46f58442f5a10cc5aa
leen <leenalgraawi@gmail.com>
2026-08-25 03:14:19 +0300

    type-annotated func

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""""
+this module will explain the annotations
+"""
+
+
+def concat(str1: string, str2: string) -> string:
+    """ concat function with type-annotated """
+    return str1 + str2
