7782172ee0e35cdf09fcedfd2863cfe197c92f93
leen <leenalgraawi@gmail.com>
2026-08-25 03:49:44 +0300

    type-annotated func

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""""
+this module will explain the annotations
+"""
+import typing
+
+
+def to_kv(k: str, mxd_lst: typing.Union[int, float]) -> tuple(str, float):
+    """
+     a type-annotated function to_kv that takes a string k
+     and an int OR float v as arguments and returns a tuple.
+     The first element of the tuple is the string k.
+     The second element is the square of the int/float v
+     and should be annotated as a float.
+    """
+    return (k, v*v)
