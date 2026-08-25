c71c9525dd02dfebf1298ea20736cef6f35fb561
leen <leenalgraawi@gmail.com>
2026-08-25 03:40:31 +0300

    type-annotated func

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""""
+this module will explain the annotations
+"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+    """
+    a type-annotated function sum_mixed_list which
+    takes a list mxd_lst of integers and floats and
+    returns their sum as a float.
+    """
+    return sum(input_list)
