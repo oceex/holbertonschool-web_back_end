ed79bdda38a4a547bd94c39979a7bb47130a67c2
leen <leenalgraawi@gmail.com>
2026-08-25 03:34:34 +0300

    type-annotated func

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""""
+this module will explain the annotations
+"""
+
+
+def sum_list(input_list: float, list) -> float:
+    """
+    a type-annotated function sum_list
+    which takes a list input_list of floats as argument
+    and returns their sum as a float.
+    """
+    return sum(input_list)
