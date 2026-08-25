7d584c9bfd9683fe765c8a027e92993a24ed3b50
leen <leenalgraawi@gmail.com>
2026-08-25 03:18:11 +0300

    type-annotated func

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""""
+this module will explain the annotations
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """ floor function with type-annotated """
+    return math.floor(n)
