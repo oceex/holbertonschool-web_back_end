1536f9ebcb5043ee40be9b73f06eb00200fd6532
leen <leenalgraawi@gmail.com>
2026-08-25 03:09:59 +0300

    type-annotated func

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+""""
+this module will explain the annotations
+"""
+
+
 def add(a: float, b: float) -> float:
     """ adding function with type-annotated """
     return a + b
