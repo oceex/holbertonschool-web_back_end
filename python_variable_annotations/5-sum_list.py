315a09ab31cb62484872118055fd88d47f8da826
leen <leenalgraawi@gmail.com>
2026-08-25 03:38:03 +0300

    type-annotated func

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """"
 this module will explain the annotations
 """
+from typing import List
 
 
-def sum_list(input_list: float, list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     a type-annotated function sum_list
     which takes a list input_list of floats as argument
