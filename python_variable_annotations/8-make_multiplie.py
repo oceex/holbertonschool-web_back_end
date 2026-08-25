791b946d059bdf3fc3e3839dd7316f902d54b57b
leen <leenalgraawi@gmail.com>
2026-08-25 03:58:26 +0300

    type-annotated func

---
+++ python_variable_annotations/8-make_multiplie.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""""
+this module will explain the annotations
+"""
+
+
+def make_multiplier(multiplier: float) -> float:
+    """
+    a type-annotated function make_multiplier that
+     takes a float multiplier as argument and returns
+      a function that multiplies a float by multiplier.
+    """
+    return multiplier*multiplier
