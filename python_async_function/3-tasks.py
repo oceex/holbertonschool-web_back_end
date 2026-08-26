b21b0c1b4d3eb2dcd4e5e544c4f21e023b825f81
leen <leenalgraawi@gmail.com>
2026-08-26 16:54:18 +0300

    k

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module with a coroutine that waits for a random delay."""
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    return asyncio.create_task(wait_random(max_delay))
