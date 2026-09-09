e4cbeff05c41e25dd9c7b4f5c24e71730f7f5942
leen <leenalgraawi@gmail.com>
2026-09-09 16:03:20 +0300

    k

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -6,8 +6,7 @@ second mudluee
 
 def insert_school(mongo_collection, **kwargs):
     """ inserts a new document in a collection based on kwargs """
-    x = mongo_collection.insert_one({"name": kwargs.get("name"), "address": kwargs.get("address")})
-
+    x = mongo_collection.insert_one(kwargs)
     return id(x)
 
 
