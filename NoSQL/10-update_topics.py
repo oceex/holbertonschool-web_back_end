1cbfd8c284fcf422fc606de2a71dc85549ffb60c
leen <leenalgraawi@gmail.com>
2026-09-09 17:31:41 +0300

    k

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -6,7 +6,7 @@ thired mudluee
 
 def update_topics(mongo_collection, name, topics):
     """" changes all topics of a school document based on the name """
-    x = mongo_collection.find_one_and_replace({"name": name} , {"topics": topics})
+    x = mongo_collection.update_many({"name": name} ,  {"$set":{"topics": topics}})
 
 
 if __name__=="__main__":
