908c6f2f3c954f28bc9bc7989f1953848d764d30
leen <leenalgraawi@gmail.com>
2026-09-09 18:00:49 +0300

    k

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -6,8 +6,7 @@ fourth mudluee
 
 def schools_by_topic(mongo_collection, topic):
     """" returns the list of school having a specific topic """
-    return list(mongo_collection.find({"topic": topic}))
-
+    return mongo_collection.find({"topics": topic})
 
 if __name__ == "__main__":
     main()
