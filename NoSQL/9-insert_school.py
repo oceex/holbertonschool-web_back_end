a533b85410fe369c86cf02d00890831367ced735
leen <leenalgraawi@gmail.com>
2026-09-09 16:10:48 +0300

    k

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -6,9 +6,20 @@ second mudluee
 
 def insert_school(mongo_collection, **kwargs):
     """ inserts a new document in a collection based on kwargs """
-    x = mongo_collection.insert_many(kwargs)
-    return id(x)
+    x = mongo_collection.insert_one(kwargs)
+    return x.inserted_id
 
+
+from pymongo import MongoClient
+list_all = __import__('8-all').list_all
+insert_school = __import__('9-insert_school').insert_school
 
 if __name__ == "__main__":
-    main()
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    school_collection = client.my_db.school
+    new_school_id = insert_school(school_collection, name="UCSF", address="505 Parnassus Ave")
+    print("New school created: {}".format(new_school_id))
+
+    schools = list_all(school_collection)
+    for school in schools:
+        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('address', "")))
