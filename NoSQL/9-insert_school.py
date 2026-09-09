b0fc2e9e8cfe2f9e6369be28f4e7a0f1b57a9bb4
leen <leenalgraawi@gmail.com>
2026-09-09 16:00:53 +0300

    k

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+second mudluee
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts a new document in a collection based on kwargs """
+    x = mongo_collection.insert_one({"name": kwargs.get("name"), "address": kwargs.get("address")})
+
+    return id(x)
+
+
+if __name__ == "__main__":
+    main()
