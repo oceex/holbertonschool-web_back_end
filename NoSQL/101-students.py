6fee80a7545e0a431d06db7d65816d269fe40a9a
leen <leenalgraawi@gmail.com>
2026-09-11 01:55:32 +0300

    k

---
+++ NoSQL/101-students.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" returns all students sorted by average score."""
+
+
+def top_students(mongo_collection):
+    students = mongo_collection.find({})
+    result = []
+    for student in students:
+        total = 0
+        count = 0
+        for topic in student["topics"]:
+            total += topic["score"]
+            count += 1
+        student["averageScore"] = total / count
+        result.append(student)
+    return sorted(result, key=lambda x: x["averageScore"], reverse=True)
