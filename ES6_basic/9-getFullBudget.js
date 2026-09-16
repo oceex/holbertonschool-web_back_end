f476dedaa730b69bd32206125d6957221cc85f27
leen <leenalgraawi@gmail.com>
2026-09-16 22:11:16 +0300

    k

---
+++ ES6_basic/9-getFullBudget.js
@@ -0,0 +1,10 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  return {
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+}
