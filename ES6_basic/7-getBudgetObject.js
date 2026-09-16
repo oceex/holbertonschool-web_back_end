e46572b827601831a064338772a6d643b3eb4c4d
leen <leenalgraawi@gmail.com>
2026-09-16 21:24:14 +0300

    k

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,3 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
-    return {"income":income, "gdp":gdp, "capita":capita};
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
 }
