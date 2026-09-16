875526af899c3678b33d20cde5f551d65912f04e
leen <leenalgraawi@gmail.com>
2026-09-16 22:30:03 +0300

    k

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array) {
-    array[idx] += appendString;
+    array[idx] = appendString + array[idx];
   }
 
   return array;
