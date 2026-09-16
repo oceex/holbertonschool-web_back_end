65759ceef59950504e7dea2e60808e8c8a4002f0
leen <leenalgraawi@gmail.com>
2026-09-16 23:02:43 +0300

    k

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {\
-    const emp =  {
-        departmentName:employees
+    return {
+        departmentName: employees,
     };
-    return emp;
 }
