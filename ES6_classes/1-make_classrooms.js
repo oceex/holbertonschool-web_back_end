2d31f3c2a7ca92252554db0ae44076177863bee4
leen <leenalgraawi@gmail.com>
2026-09-19 17:57:43 +0300

    k

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,4 +1,4 @@
 import ClassRoom from "./0-classroom.js";
 function initializeRooms(){
-    return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)]
+    return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
