7fa8bd51fae29b79dfe37103d349274a3c202fed
leen <leenalgraawi@gmail.com>
2026-09-20 16:43:29 +0300

    k

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -4,6 +4,8 @@ export default function cleanSet(set, startString) {
   if (!startString) {
     return '';
   }
+  else if (typeof startString !== 'Sting')
+      return '';
 
   set.forEach((value) => {
     if (value.startsWith(startString)) {
