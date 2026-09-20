2cc600e406ff9a77b65f1f31aee33278dfb951c7
leen <leenalgraawi@gmail.com>
2026-09-20 11:13:32 +0300

    k

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,8 @@
 function getListStudents(){
-    return Array(
+    const k = Array(
   { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
   { id: 2, firstName: 'James', location: 'Columbia' },
   { id: 5, firstName: 'Serena', location: 'San Francisco' }
     );
+    return k;
 }
