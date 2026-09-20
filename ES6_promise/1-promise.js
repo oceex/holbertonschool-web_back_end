bc656178ef1d88200147ce77af8d954bea370a4a
leen <leenalgraawi@gmail.com>
2026-09-20 23:51:52 +0300

    k

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,14 +1,14 @@
-export default async function getFullResponseFromAPI(success){
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-          if (success) {
-              resolve({
-                  status: 200,
-                  body: 'Success',
-              });
-          } else {
-              reject("The fake API is not working currently");
-          }
-      });
-    });
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (success) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    }, 1000);
+  });
 }
