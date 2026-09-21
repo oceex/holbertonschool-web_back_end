3d5d0e247efc430e4c7e44ebfa8730329472a343
leen <leenalgraawi@gmail.com>
2026-09-22 02:28:13 +0300

    k

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,7 @@
 export default function uploadPhoto(filename) {
-    return new Promise((reject) => {
+    return new Promise((resolve, reject) => {
         reject(new Error(`${filename} cannot be processed`))
     });
 }
+
+console.log(uploadPhoto('guillaume.jpg'));
