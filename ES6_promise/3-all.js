74165b6656868027f6c3a01a285dee5fec231c35
leen <leenalgraawi@gmail.com>
2026-09-22 02:14:47 +0300

    k

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from './utils.js'
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup(){
     Promise.all([uploadPhoto(), createUser()])
         .then(([y, x]) => {
-            console.log(`${y.body} ${x.firstName} ${x.lastName}`)
+            console.log(`${y.body} ${x.firstName} ${x.lastName}`);
         })
         .catch(() => {
-            console.log("Signup system offline")
-        })
+            console.log("Signup system offline");
+        });
 }
