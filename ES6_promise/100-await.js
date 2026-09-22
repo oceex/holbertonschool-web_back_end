2dd98b3e00725a08d3b9a46e47f3aa93ecc1898a
leen <leenalgraawi@gmail.com>
2026-09-22 17:29:43 +0300

    k

---
+++ ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import {uploadPhoto, createUser} from './utils.js'
+
+export default async function asyncUploadUser(){
+    return Promise.all([uploadPhoto, createUser]).then((res, rej) =>{
+        return {
+          photo: res[0],
+          user: res[1],
+        }
+    }).catch((e) => {
+        return {
+          photo: null,
+          user: null,
+        }
+    });
+}
