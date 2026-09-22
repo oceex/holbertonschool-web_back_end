4db13a531b429504d4cffb09dff1042db86d85d1
leen <leenalgraawi@gmail.com>
2026-09-22 17:31:40 +0300

    k

--- ES6_promise/100-await.js
+++ ES6_promise/100-await.js
@@ -1,7 +1,7 @@
 import {uploadPhoto, createUser} from './utils.js'
 
 export default async function asyncUploadUser(){
-    return Promise.all([uploadPhoto, createUser]).then((res, rej) =>{
+    return Promise.all([uploadPhoto, createUser]).then((res) =>{
         return {
           photo: res[0],
           user: res[1],
